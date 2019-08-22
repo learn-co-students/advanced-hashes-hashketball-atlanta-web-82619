702ba985c4856b34733a8404e6527a30967601d1
Dan Reeves <djreeves1292@gmail.com>
2019-08-22 15:13:06 +0000

    Done.

--- hashketball.rb
+++ hashketball.rb
@@ -1,5 +1,17 @@
-# Write your code here!
-
+def game_hash
+  top_level = {
+    :home=> {
+      :team_name=> "Brooklyn Nets",
+      colors: ["Black", "White"],
+      :players=> [:number=>{}, :shoe=>{}, :points=>{}, :rebounds=>{}, :assists=>{}, :steals=>{}, :blocks=>{}, :slam_dunks=>{}]
+      
+    }
+    :away=> {
+     :team_name=> "Charlotte Hornets",
+      :colors=> ["Turquoise", "Purple"],
+      :players=> [:number=>{}, :shoe=>{}, :points=>{}, :rebounds=>{}, :assists=>{}, :steals=>{}, :blocks=>{}, :slam_dunks=>{}]
+}
+end
 
 
 
